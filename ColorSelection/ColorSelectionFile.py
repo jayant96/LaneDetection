3d40ac5da9ba111933e6a30830193a2e93637cc8
Jayant Sarkar <jayant96@gmail.com>
2017-01-09 09:24:34 -0800

    first commit

---
+++ ColorSelection/ColorSelectionFile.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image and print out some stats
+# Note: in the previous example we were reading a .jpg
+# Here we read a .png and convert to 0,255 bytescale
+image = (mpimg.imread('test.png') * 255).astype('uint8')
+print('This image is: ', type(image),
+      'with dimensions:', image.shape)
+
+# Grab the x and y size and make a copy of the image
+ysize = image.shape[0]
+xsize = image.shape[1]
+color_select = np.copy(image)
+
+# Define color selection criteria
+###### MODIFY THESE VARIABLES TO MAKE YOUR COLOR SELECTION
+red_threshold = 198
+green_threshold = 198
+blue_threshold = 203
+######
+
+rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+
+# Do a boolean or with the "|" character to identify
+# pixels below the thresholds
+thresholds = (image[:, :, 0] < rgb_threshold[0]) \
+             | (image[:, :, 1] < rgb_threshold[1]) \
+             | (image[:, :, 2] < rgb_threshold[2])
+color_select[thresholds] = [0, 0, 0]
+plt.imshow(color_select)
+# Display the image
+plt.imshow(color_select)
+plt.show()  #show image in local machine
+
+# Uncomment the following code if you are running the code locally and wish to save the image
+mpimg.imsave("test-after-good.png", color_select)
+
+# to show resulted image in jupyter notebook
+# from IPython.display import Image
+
+# i = Image(filename='test-after.jpg')
+# i
